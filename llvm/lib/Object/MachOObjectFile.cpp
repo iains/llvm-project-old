0b93c691ad1c4434ea3b6d0df11252d647bac16f
Iain Sandoe <iain@codesourcery.com>
2014-04-24 01:20:41 +0100

    [llvm, Darwin, ppc] Improve support for Mach-O relocs.
    Scattered data relocs are needed to implement EH (as a minimum) plus there are a number of incorrect code-gen cases in the existing implementation.
    
    Essentially, other than adding support for .quad, Darwin ppc64 relocs are the same as ppc32 ones, so there’s no need to exclude ppc64 from MC layer (at least).
    
    Amend MachOObjectFile and tests to reflect this.
    Fix symbol+offset case for H* relocs.
    Add tests.

--- llvm/lib/Object/MachOObjectFile.cpp
+++ llvm/lib/Object/MachOObjectFile.cpp
@@ -1986,6 +1986,7 @@ void MachOObjectFile::getRelocationTypeName(
         res = Table[RType];
       break;
     }
+    case Triple::ppc64: // PPC64 and PPC share relocations.
     case Triple::ppc: {
       static const char *const Table[] =  {
         "PPC_RELOC_VANILLA",
